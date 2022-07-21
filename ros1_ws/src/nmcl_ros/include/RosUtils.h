86fa756679b550c6828996e4f981183d22b35eb9
FullMetalNicky <nickyfullmetal@gmail.com>
2022-07-21 14:13:54 +0200

    remove unused Box msg function

--- ros1_ws/src/nmcl_ros/include/RosUtils.h
+++ ros1_ws/src/nmcl_ros/include/RosUtils.h
@@ -22,7 +22,7 @@
 #include <geometry_msgs/PoseStamped.h>
 #include "opencv2/opencv.hpp"
 #include <cv_bridge/cv_bridge.h>
-#include <nmcl_msgs/Box.h>
+
 
 
 Eigen::Vector3f OdomMsg2Pose2D(const nav_msgs::OdometryConstPtr& odom);
@@ -48,7 +48,7 @@ geometry_msgs::PoseWithCovarianceStamped Pred2PoseWithCov(Eigen::Vector3d pred, 
 
 sensor_msgs::Image CVMat2ImgMsg(const cv::Mat& img, std_msgs::Header header, const std::string& type);
 
-nmcl_msgs::Box CVRect2BoxMsg(const cv::Rect& rect);
+
 
 
 
